7a45b6aa99acfc76277d8ef2c0067c1bebc08050
lshyun955 <lshyun955@gmail.com>
2022-06-24 17:37:34 +0900

    Fix: gitignore database폴더 추가

--- database/schema/08.create_rooms_intro.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE rooms_intro(
-  id int auto_increment primary key,
-  rooms_id int not null,
-  title varchar(300),
-  main_content text,
-  sub_content text,
-  foreign key(rooms_id) references rooms(id) on delete cascade
-);
