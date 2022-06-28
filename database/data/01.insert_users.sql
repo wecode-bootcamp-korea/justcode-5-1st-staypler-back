5f734a7773da0f7235ae6dffc906cbe412ec3ac6
장태준 <jangtaejun@jangtaejun-ui-MacBookAir.local>
2022-06-28 13:56:39 +0900

    불필요한 폴더 삭제

--- database/data/01.insert_users.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO users(email,name,password,phone) VALUES('123@naver.com','lee','12345678','010-0000-0000');
-INSERT INTO users(email,name,password,phone) VALUES('111@gmail.com','kim','12345678','010-0000-0001');
-INSERT INTO users(email,name,password,phone) VALUES('445@naver.com','park','12345678','010-0000-0002');
-INSERT INTO users(email,name,password,phone) VALUES('145@gmail.com','lee','12345678','010-0000-0003');
-INSERT INTO users(email,name,password,phone) VALUES('999@naver.com','kim','12345678','010-0000-0004');
-INSERT INTO users(email,name,password,phone) VALUES('555@gmail.com','park','12345678','010-0000-0005');
-INSERT INTO users(email,name,password,phone) VALUES('321@naver.com','lee','12345678','010-0000-0006');
