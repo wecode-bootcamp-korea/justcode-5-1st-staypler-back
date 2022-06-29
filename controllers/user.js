ac6d7a756f1efd2d5cee2ad02b66f371fba79e83
lshyun955 <lshyun955@gmail.com>
2022-06-29 16:29:45 +0900

    Fix: 인증 미들웨어 수정

--- controllers/user.js
+++ controllers/user.js
@@ -1,50 +1,52 @@
 import * as userService from '../services/user.js';
 
+export const validateUser = async (req, res, next) => {
+  const { email, password, username, phoneNumber } = req.body;
 
-  export const validateUser = async (req, res, next) => {
-    const { email, password, username, phoneNumber } = req.body;
-
-    if (!email || !password || !username || !phoneNumber) {
-      res.status(400).json({ message: 'KEY_ERROR' });
-      return;
-    }
-    next();
+  if (!email || !password || !username || !phoneNumber) {
+    res.status(400).json({ message: 'KEY_ERROR' });
+    return;
   }
+  next();
+};
 
 //회원가입
 //controller에서는 오직 request 검증
-  export const signupController = async (req, res) => {
-    try {
-      const {email, username, password, phoneNumber} =
-        req.body;
-        console.log(req.body);
-  
-  
-        await userService.signup(email, username, password, phoneNumber);
-        return res.status(201).json({
-          message: 'SIGNUP_SUCCESS',
-        });
-    } catch (err) {
-      console.log(err);
-      return res.status(err.statusCode || 500).json({ message: err.message });
-    }
-  };
+export const signupController = async (req, res) => {
+  try {
+    const { email, username, password, phoneNumber } = req.body;
+
+    await userService.signup(email, username, password, phoneNumber);
+    return res.status(201).json({
+      message: 'SIGNUP_SUCCESS',
+    });
+  } catch (err) {
+    return res.status(err.statusCode || 500).json({ message: err.message });
+  }
+};
 
 //로그인
 
-  export const loginController = async (req, res) => {
-    try {
-      const { email, password } = req.body;
-  
-      
-      const token = await userService.logIn(email, password);
-  
-      return res.status(201).json({
-        message: 'LOGIN_SUCCESS',
-        token: token,
-      });
-    } catch (err) {
-      console.log(err);
-      return res.status(err.statusCode || 500).json({ message: err.message });
-    }
-  };
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const token = await userService.logIn(email, password);
+
+    return res.status(201).json({
+      message: 'LOGIN_SUCCESS',
+      token: token,
+    });
+  } catch (err) {
+    return res.status(err.statusCode || 500).json({ message: err.message });
+  }
+};
+
+export const me = async (req, res) => {
+  try {
+    await userService.me(req.userId);
+    res.status(200).json({ token: req.token });
+  } catch (error) {
+    res.status(error.statusCode || 500).json({ message: error.message });
+  }
+};
