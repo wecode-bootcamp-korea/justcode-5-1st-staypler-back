ae65cf65b1afd1d238119c8c083220ec5737a5c7
lshyun955 <lshyun955@gmail.com>
2022-07-04 16:12:24 +0900

    Fix: 숙소 리스트  날짜 필터링 부분 쿼리 수정

--- models/room.js
+++ models/room.js
@@ -75,17 +75,14 @@ function generateHavingStatement({
   return havingArray.length ? `HAVING ${havingArray.join(' and ')}` : '';
 }
 
-function generateJoinDateStatement(date) {
-  return date
-    ? `JOIN (SELECT room_type.id,room_type.rooms_id,room_type.max_limit,room_type.min_limit,room_type.price
-    FROM room_type
-    LEFT JOIN reservation
-    ON room_type.id = reservation.room_type_id
-    WHERE reservation.start_date
-    NOT BETWEEN '${date.start_date}' AND '${date.end_date}'
-    AND reservation.end_date
-    NOT BETWEEN '${date.start_date}' AND '${date.end_date}'
-    OR reservation.start_date IS NULL AND reservation.end_date IS NULL) room_type
+function generateJoinDateStatement(start_date, end_date) {
+  return start_date && end_date
+    ? `JOIN (SELECT * 
+      FROM (SELECT * 
+        FROM room_type 
+        WHERE room_type.id NOT IN (SELECT reservation.room_type_id 
+          FROM reservation 
+          WHERE ( '${start_date}' < reservation.end_date ) AND ( reservation.start_date < '${end_date}' ))) rtype) room_type
     ON rooms.id = room_type.rooms_id`
     : `JOIN room_type ON rooms.id = room_type.rooms_id`;
 }
@@ -133,7 +130,38 @@ export async function readById(userId, roomId, date) {
   ON rooms.id = rs.rooms_id
   WHERE rooms.id=${roomId}
   GROUP BY rooms.id, rooms_intro.id ${userId ? `, rooms_like.id` : ``}`);
-
+  const sql = `SELECT
+  rooms.title room_name,
+  rooms.province,
+  rooms.city,
+  rooms.concept,
+  rt.room,
+  ri.images,
+  rs.specials,
+  JSON_OBJECT('id',rooms_intro.id,'title ',rooms_intro.title, 'main_content',rooms_intro.main_content,'sub_content',rooms_intro.sub_content) intro,
+  ${userId ? `IFNULL(rooms_like.isLike,0) islike,` : ``}
+  rooms.address
+  
+  FROM (SELECT id, title, concept, address, province, city FROM rooms GROUP BY id) rooms
+  LEFT JOIN (SELECT rooms_image.rooms_id,JSON_ARRAYAGG(CASE WHEN rooms_image.id IS NOT NULL AND rooms_image.image IS NOT NULL THEN rooms_image.image END)  images FROM rooms_image GROUP BY rooms_image.rooms_id) ri
+  ON rooms.id = ri.rooms_id
+  LEFT JOIN (SELECT room_type.rooms_id,JSON_ARRAYAGG(CASE WHEN room_type.id IS NOT NULL THEN JSON_OBJECT('id',room_type.id,'title',room_type.title,'type',room_type.type,'price',room_type.price,'image',rti.image,'max_limit',max_limit,'min_limit',min_limit) END) room FROM room_type LEFT JOIN (SELECT room_type_id,image FROM room_type_image GROUP BY id ORDER BY room_type_id limit 1) rti ON rti.room_type_id=room_type.id ${generateJoinStatement(
+    date
+  )} GROUP BY room_type.rooms_id) rt
+  ON rt.rooms_id = rooms.id
+  ${
+    userId
+      ? `LEFT JOIN (SELECT id,rooms_id, isLike FROM likes WHERE user_id=${userId} GROUP BY id) as rooms_like
+  ON rooms_like.rooms_id = rooms.id`
+      : ``
+  }
+  JOIN (SELECT id,rooms_id,title,main_content,sub_content FROM rooms_intro GROUP BY id) rooms_intro
+  ON rooms.id = rooms_intro.rooms_id
+  JOIN (SELECT rooms_special.rooms_id,JSON_ARRAYAGG(CASE WHEN rooms_special.id IS NOT NULL THEN JSON_OBJECT('id',rooms_special.id,'title',rooms_special.title,'content',rooms_special.content,'image',rooms_special.image) END) specials FROM rooms_special GROUP BY rooms_special.rooms_id) rs
+  ON rooms.id = rs.rooms_id
+  WHERE rooms.id=${roomId}
+  GROUP BY rooms.id, rooms_intro.id ${userId ? `, rooms_like.id` : ``}`;
+  console.log(sql);
   return roomInfo;
 }
 
@@ -141,10 +169,7 @@ function generateJoinStatement(date) {
   return !!(date.start_date && date.end_date)
     ? `LEFT JOIN reservation
   ON reservation.room_type_id = room_type.id
-WHERE reservation.start_date
-NOT BETWEEN '${date.start_date}' AND '${date.end_date}'
-AND reservation.end_date
-NOT BETWEEN '${date.start_date}' AND '${date.end_date}'
+WHERE ( '${date.start_date}' < reservation.end_date ) AND ( reservation.start_date < '${date.end_date}' )
 OR reservation.start_date IS NULL AND reservation.end_date IS NULL`
     : ``;
 }
@@ -198,10 +223,7 @@ export async function roomCheck(id) {
 
 const generateWhereStatement = date => {
   return date.start_date && date.end_date
-    ? `WHERE reservation.start_date
-  NOT BETWEEN '${date.start_date}' AND '${date.end_date}'
-AND reservation.end_date
-    NOT BETWEEN '${date.start_date}' AND '${date.end_date}'
+    ? `WHERE ( '${start_date}' < reservation.end_date ) AND ( reservation.start_date < '${end_date}' )
 OR reservation.start_date IS NULL AND reservation.end_date IS NULL`
     : ``;
 };
